472b5203fe1d5f57b0b299eff7883dab847d856b
ugurmert <kdrugurmrt@gmail.com>
2021-03-01 02:01:39 +0300

    City özelliğinin düzeltilmesi

--- ClassMetotDemo/Product.cs
+++ ClassMetotDemo/Product.cs
@@ -7,10 +7,10 @@ namespace ClassMetotDemo
     class Product
     {
         public int Id { get; set; }
-        public int CityId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public double AccountBalance { get; set; }
         public string Phone { get; set; }
+        public int City { get; set; }
     }
 }
